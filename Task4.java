b074f1532f4a1d8df587fcfcf5ff1a28c05c09a7
Ameya Mataghare <75973515+ameya24@users.noreply.github.com>
2021-03-13 05:11:13 +0530

    Task 4
    Wap in java to check whether a number is a prime number or not.

---
+++ Task4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Task4 
+{
+
+  public static void main()
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter any number");
+    int num = sc.nextInt();
+    int flag = 0;
+    for (int i = 2; i <= num / 2; ++i) {
+      // condition for nonprime number
+      if (num % i == 0) {
+        flag++;
+        break;
+      }
+    }
+
+    if (flag==0)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+  }
+}
